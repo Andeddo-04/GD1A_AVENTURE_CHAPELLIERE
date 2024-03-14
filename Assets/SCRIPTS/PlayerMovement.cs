5a55c22fde53a1d115a0da5343b50337cb6811d1
CHAPELLIERE Youenn <youennchapi@gmail.com>
2024-03-14 16:20:55 +0100

    init project
    Initialisation du projet avec l'importation d'éléments déjà prêt.

---
+++ Assets/SCRIPTS/PlayerMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float horizontalMovement, verticalMovement;
+
+    [SerializeField] private float moveSpeed;
+
+    [SerializeField] private Rigidbody2D characterSprite;
+
+    [SerializeField] private SpriteRenderer characterSpriteRenderer;
+
+    [SerializeField] private Vector3 velocity = Vector3.zero;
+
+    private Vector2 lastMoveDirection = Vector2.right; // Store last move direction
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        MovePlayer(horizontalMovement, verticalMovement);
+
+        // Flip the character based on last move direction
+        if (horizontalMovement != 0)
+        {
+            lastMoveDirection = new Vector2(horizontalMovement, 0f);
+            Flip();
+        }
+    }
+
+    void MovePlayer(float _horizontalMovement, float _verticalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticalMovement);
+        characterSprite.velocity = Vector3.SmoothDamp(characterSprite.velocity, targetVelocity, ref velocity, 0.05f);
+    }
+
+    void Flip()
+    {
+        if (lastMoveDirection.x < 0)
+        {
+            characterSpriteRenderer.flipX = false;
+        }
+        else if (lastMoveDirection.x > 0)
+        {
+            characterSpriteRenderer.flipX = true;
+        }
+    }
+}
