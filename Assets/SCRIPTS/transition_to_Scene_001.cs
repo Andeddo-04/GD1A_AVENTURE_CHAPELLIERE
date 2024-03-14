d98ac3491070e3c2d6a27d7a55d20215d41185b8
CHAPELLIERE Youenn <youennchapi@gmail.com>
2024-03-14 16:56:24 +0100

    add transition_to_Scene_001 script
    Ajout du script "transition_to_Scene_001".
    
    Script non fonctionel.

---
+++ Assets/SCRIPTS/transition_to_Scene_001.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadSpecificScene : MonoBehaviour
+{
+    public string sceneName;
+
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            StartCoroutine(loadNextScene());
+        }
+    }
+
+    public IEnumerator loadNextScene()
+    {
+        LoadAndSaveData.instance.SaveData();
+        SceneManager.LoadScene(sceneName);
+    }
+}
