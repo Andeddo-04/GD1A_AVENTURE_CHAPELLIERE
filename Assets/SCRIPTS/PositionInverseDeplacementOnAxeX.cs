401a6f5aaf3cc6376eb3064b4deaa785f2ea1d21
CHAPELLIERE Youenn <youennchapi@gmail.com>
2024-03-18 09:45:11 +0100

    léger refactoring

--- Assets/SCRIPTS/PositionInverseDeplacementOnAxeX.cs
+++ Assets/SCRIPTS/PositionInverseDeplacementOnAxeX.cs
@@ -7,14 +7,11 @@ public class PositionInverseDeplacementOnAxeX : MonoBehaviour
 
     void Update()
     {
-        // Récupère la position x du joueur
-        float playerPositionX = player.position.x;
+        // Récupère la position x et y du joueur
+        float playerPositionX = player.position.x, playerPositionY = player.position.y;
 
         // Calcule l'inverse de la position x du joueur pour l'élément à inverser
         float inversePositionX = -playerPositionX;
-
-        // Récupère la position y du joueur
-        float playerPositionY = player.position.y;
 
         // Modifie directement la position x de l'élément avec l'inverse de la position x du joueur
         // Modifie la position y de l'élément avec la position y du joueur
