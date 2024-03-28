4733930346867bd330f08be8f7827fe785912014
CHAPELLIERE Youenn <youennchapi@gmail.com>
2024-03-28 16:53:31 +0100

    ajout d'un "peux" de police d'écriture

---
+++ Assets/SCRIPTS/logistic's scripts/Cristal.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cristal : MonoBehaviour
+{
+    private Text interactUI;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        interactUI = GameObject.FindGamebjectWithTag("InteractUI").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.enabled = true
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            interactUI.enabled = false;
+        }
+    }
+}
